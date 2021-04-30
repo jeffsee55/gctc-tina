cd28049bb79df69a3926fa8849967d8b48a28668
Jeff See <jeffsee.55@gmail.com>
2021-04-30 16:03:40 -0700

    more refactoring

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,50 +1,56 @@
 import "../styles/globals.css";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import Link from "next/link";
 import dynamic from "next/dynamic";
 
-function MyApp({ Component, pageProps }) {
-  const { route } = useRouter();
-  if (pageProps.preview) {
+import { EditProvider, useEditState } from "../util/edit-state";
+
+function InnerApp({ Component, pageProps }) {
+  const { edit } = useEditState();
+  if (edit) {
     const TinaWrapper = dynamic(() => import("../components/tina-wrapper"));
     return (
       <>
-        <Head>
-          {route.startsWith("/training-plans") && (
-            <script src="https://js.stripe.com/v3/"></script>
-          )}
-        </Head>
         <TinaWrapper {...pageProps}>
           {(props) => <Component {...props} />}
         </TinaWrapper>
-        <EditToggle isInEditMode={true} />
+        <EditToggle />
       </>
     );
   }
   return (
     <>
+      <Component {...pageProps} />
+      <EditToggle />
+    </>
+  );
+}
+
+function App(props) {
+  const { route } = useRouter();
+  return (
+    <EditProvider>
       <Head>
         {route.startsWith("/training-plans") && (
           <script src="https://js.stripe.com/v3/"></script>
         )}
       </Head>
-      <Component {...pageProps} />
-      <EditToggle isInEditMode={false} />
-    </>
+      <InnerApp {...props} />
+    </EditProvider>
   );
 }
+export default App;
 
-export default MyApp;
-
-const EditToggle = ({ isInEditMode }: { isInEditMode: boolean }) => {
+const EditToggle = () => {
+  const { edit, setEdit } = useEditState();
   return (
     <div className="absolute top-6 right-4 z-50">
-      <Link href={`/api/preview`}>
-        <a className="cursor-pointer relative bg-steel-medium text-white rounded-lg shadow-md p-4">
-          {isInEditMode ? "Exit edit mode" : "Enter edit mode"}
-        </a>
-      </Link>
+      <button
+        onClick={() => setEdit(!edit)}
+        className="cursor-pointer relative bg-steel-medium text-white rounded-lg shadow-md p-4"
+      >
+        {edit ? "Exit edit mode" : "Enter edit mode"}
+      </button>
     </div>
   );
 };
